5770ed7f6c6ed912f01cd3f78b35a07675c4efe8
grguzman <55497579+grguzman@users.noreply.github.com>
2019-11-21 11:57:59 -0500

    Add files via upload

--- src/reverse.js
+++ src/reverse.js
@@ -1,20 +1,22 @@
 const readlineSync = require("readline-sync");
 
-let MAX = MAX_SAFE_INTEGER
-let MIN = 1
-
-let positiveinteger = -1
-
+let MAX = Number.MAX_SAFE_INTEGER;
+let MIN = 1;
+let pI = -1
 console.log();
 
-while (positiveinteger > 0 || Number.NaN(positiveinteger) || Number.isInteger(positiveinteger) || positiveinteger < MIN || positiveinteger > MAX){
-  positiveinteger = Number(readlineSync("Positive Integer: "));
+while (pI <= 0 || Number.isNaN(pI) || !Number.isInteger(pI) || pI < MIN || pI > MAX){
+  pI = Number(readlineSync.question("Positive Integer: "));
 }
  let str = "";
 
-while(positiveinteger > 0)
-  if 
-
+while(pI > 0){
+  let number = (pI % 10);
+  pI = Math.floor(pI / 10);
+  if (pI > 0) {
+    str = str + number + ", ";
+  } else {
+    str = str + number + "."
+  }
 }
-
-}
+console.log("\n" + str);
