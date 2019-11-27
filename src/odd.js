78008dff7a81f3d3746ae8a03dee759a6926e518
grguzman <55497579+grguzman@users.noreply.github.com>
2019-11-27 00:05:51 -0500

    Add files via upload

--- src/odd.js
+++ src/odd.js
@@ -1,1 +1,22 @@
 const readlineSync = require("readline-sync");
+
+const MAX = Number.MAX_SAFE_INTEGER;
+
+const MIN = 1;
+
+let digits = 0;
+
+let pI = Number(readlineSync.question("\nPositive Integer: "));
+
+let s = 0;
+while (pI < MIN || pI > MAX || Number.isNaN(pI) || pI % 1 != 0) {
+  pI = Number(readlineSync.question("Positive Integer: "));
+}
+while (pI != 0) {
+  digits = pI % 10;
+  pI = Math.floor(pI / 10);
+  if (digits % 2 != 0) {
+    s = s + digits
+  }
+}
+console.log("\n" + s + ".\n");
